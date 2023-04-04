69e38713d044fbe1dd59f7ce94aa3ebf4fb5e235
Keith Brings <keith.brings@noizu.com>
2023-04-04 08:50:34 +0700

    Initial Commit

---
+++ lib/noizu_labs_clusters.ex
@@ -0,0 +1,18 @@
+defmodule NoizuLabsClusters do
+  @moduledoc """
+  Documentation for `NoizuLabsClusters`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NoizuLabsClusters.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/noizu_labs_clusters_test.exs
@@ -0,0 +1,8 @@
+defmodule NoizuLabsClustersTest do
+  use ExUnit.Case
+  doctest NoizuLabsClusters
+
+  test "greets the world" do
+    assert NoizuLabsClusters.hello() == :world
+  end
+end
